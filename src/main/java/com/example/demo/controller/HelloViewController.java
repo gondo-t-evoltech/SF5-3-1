d37dbbb248cda8b9568bd651f0fbbb97bad25d28
gon <gondo-t@evoltech.co.jp>
2026-06-04 15:41:53 +0900

    HellowViewControllerでリクエストハンドラメソッドの引数にModel型を追加

--- src/main/java/com/example/demo/controller/HelloViewController.java
+++ src/main/java/com/example/demo/controller/HelloViewController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("hello")
 public class HelloViewController {
 	
-	@GetMapping("view")
-	public String helloView() {
+	@GetMapping("model")
+	public String helloView(Model model) {
+		// 「Model」にデータを格納する
+		model.addAttribute("msg", "タイムリーフ!!!");
 		// 戻り値は「ビュー名」を返す
-		return "hello";
+		return "helloThymeleaf";
 	}
 }
